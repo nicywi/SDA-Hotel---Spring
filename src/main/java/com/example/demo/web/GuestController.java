e79ce3ecd10458d2a307004250504c49b6ec6691
Nikola Cywinska <nicywi@gmail.com>
2023-10-15 14:34:35 +0200

    init 3

--- src/main/java/com/example/demo/web/GuestController.java
+++ src/main/java/com/example/demo/web/GuestController.java
@@ -3,6 +3,7 @@ package com.example.demo.web;
 import com.example.demo.service.ReservationService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,8 +16,9 @@ public class GuestController {
         this.reservationService = reservationService;
     }
 
+    @GetMapping
     public String getGuests(Model model){
-        model.addAttribute(reservationService.getHotelGuests());
+        model.addAttribute("guests", reservationService.getHotelGuests());
         return "guests";
     }
 }
